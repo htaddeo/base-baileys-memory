864b9fffc5d9efcef52a44478fe22520d1c782af
htaddeo <htaddeo@hotmail.com>
2025-04-03 10:25:24 -0300

    commit

--- flows/welcome.flow.js
+++ flows/welcome.flow.js
@@ -3,7 +3,7 @@ const { addKeyword, EVENTS } = require('@bot-whatsapp/bot');
 const welcomeFlow = addKeyword(EVENTS.ACTION)
     .addAction(async (ctx, ctxFn) => {
 //        await ctxFn.endFlow("¡Bienvenido!😊 Soy Somi, tu asistente Virtual. Nos alegra mucho tenerte aquí. Esperamos que disfrutes tu experiencia y que encuentres todo lo que necesitas, Podes escribir 'Menu' para ver las opciones disponibles")    
-        await ctxFn.endFlow(" Hola! Soy Somi!😊 ¿En qué puedo ayudarte? Escribe Menu para que puedas ver las opciones");
+        await ctxFn.endFlow(" ¡Hola! Soy Somi! 😊¿En qué puedo ayudarte?\n Escribe 'Menú' para ver las opciones disponibles. 📋");
     })
 
 module.exports = { welcomeFlow };
